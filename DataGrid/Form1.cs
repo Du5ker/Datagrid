5cb4c9a72241beb0544243ec52ac4ac6283fc1c7
Du5ker <examerpley@gmail.com>
2023-09-18 13:09:00 +0300

    Vtwo

--- DataGrid/Form1.cs
+++ DataGrid/Form1.cs
@@ -49,5 +49,43 @@ namespace DataGrid
         {
             FillDataGridView();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShowSecondRow();
+        }
+
+        private void ShowSecondRow()
+        {
+            string connectionString = "Data Source=database.sqlite;Version=3;";
+            string query = "SELECT * FROM MyTable LIMIT 1 OFFSET 1;"; // Замените "YourTableName" на имя вашей таблицы
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            dataGridView1.DataSource = dataTable;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No data found in the table.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
+        }
     }
 }
